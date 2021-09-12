b65f3a68108af55bc0fcb7b33b1adfd4185b58f5
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-09-12 19:05:20 +0300

    Added methods to take in player input, compare player input with the secret code, to update the game board appropriately, and to check for win condition.

--- mastermind.rb
+++ mastermind.rb
@@ -1,66 +1,97 @@
 require 'pry-byebug'
-#a row of 4 holes
-#twelve rows of 4 holes next to a set of 4 holes
-#6 different colors
-#2 different pegs black and white placed in small holes
-
-
-#A PATTERN OF 4 RANDOM COLORS IS CHOSEN
-
-class GAME
+class MASTERMIND
     attr_reader :board, :code_row, :main_colors
     def initialize
         @main_colors = ["red","yellow","blue","purple","pink","teal"]
-        @secondary_colors = ["black","white"]
-        code_maker_ai()
     end
-private
-#creates two 1 by 4 rows
-    def rows
-        row_uno = Array.new(4)
-        row_dos = Array.new(4)
-    end
-#takes the rows() and makes a game board
-    def board
-        @board = Array.new(12) { Array.new(2) { rows() } }
+public
+#creates the game board
+    def create_board
+        temp_index = 0
+        temp_column1 = Array.new(4) {"O"}
+        temp_column2 = Array.new(4) {"O"}
+        @board = [] 
+        12.times do 
+            @board << [temp_column1,temp_column2,"Turn:#{temp_index+=1}"]
+        end
+        @board
     end
 #selects 4 random colors to stand for the code
-def code_maker_ai
-    ai_colors = []
-     ai_colors += @main_colors
-    @code_row = []
-    4.times do
-        temp_color = ai_colors.sample
-        ai_colors.delete(temp_color)
-        @code_row << temp_color
+    def secret_code
+        ai_colors = Array.new(1) {@main_colors}.flatten
+        @code_row = []
+        4.times do
+            temp_color = ai_colors.sample
+            ai_colors.delete(temp_color)
+            @code_row << temp_color
+        end
     end
-end
-
-public
-
-    def compare_guess(player_input)
-        matches = []
+    def validate_player_input
+        @player_input = []
+        @available_colors = Array.new(1) {@main_colors}
+            4.times do
+            temp_input = request_player_input()
+            unless available_colors.index(temp_input).class == Integer
+                request_player_input()
+            else
+                store_player_input(temp_input)
+            end
+        end
+    end
+    def store_player_input(temp_input)
+        @player_input << available_colors[available_colors.index(temp_input)]
+        @available_colors.delete(temp_input)
+    end
+    def request_player_input
+        puts "Choose from one of the following colors"
+        p @available_colors
+        temp_input = gets.chomp
+    end
+    def correct_colors
+        @matches = []
         temp_row = [] 
         temp_row += @code_row
-        player_input.each do |item|
+        @player_input.each do |item|
             if temp_row.include?(item)
-                matches << @secondary_colors[1]
+                @matches << "white"
                 temp_row.delete_at(temp_row.index(item))
             end
         end
-        matches
+    end
+    def correct_positions
         @code_row.each_with_index do |item,index|
-            if @code_row[index] == player_input[index]
-                matches.shift
-                matches<<@secondary_colors[0]
+            if @code_row[index] == @player_input[index]
+                @matches.shift
+                @matches<< "black"
             end
         end
-        matches
     end
-
-
+    def compare_guesses
+        correct_colors()
+        correct_positions()
+        @matches
+    end
+    def update_board(turn)
+        @board[0][0]=@matches
+        @board[0][1]=@player_input
+    end
+    def check_for_win_condition
+        @matches.all?("black")
+    end
+    # def game(turn)
+    #     get_player_input()
+    #     unless check_for_win_condition() == true
+    #         @board[turn][0] = @matches
+    #     end
+    # end
 end
-game_board = GAME.new()
+
+game = MASTERMIND.new()
 binding.pry
+# turns = 1
+# while turns<=12
+# game.game(turns)
+# end
+
 
 p "Something"
