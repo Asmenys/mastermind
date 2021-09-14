696f21da614c22542c9e6e7681dfbd5693856040
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-09-14 19:34:48 +0300

    Changed up initialize to allow for different player roles, validate_player_input allows for different types of inputs now, added bot_turn method to use when playing as the code maker

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,17 @@
 require 'pry-byebug'
+
 class MASTERMIND
     attr_reader :board, :code_row, :MAIN_COLORS
 #creates the game board with some constants
-    def initialize
+    def initialize(player_role = "code breaker")
         @MAIN_COLORS = ["red","yellow","blue","purple","pink","teal"]
         create_board()
-        secret_code()
+        if player_role == "code maker"
+            get_player_input()
+            @code_row = Array.new(1) {@code_breaker_input}.flatten
+        else
+            secret_code()
+        end
     end
 public
 #creates the game board
@@ -29,9 +35,14 @@ public
             @code_row << temp_color
         end
     end
+#! WRITE THIS LATER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    def get_bot_input
+
+    end
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #this calls the request_player_input to take the player input, then validates it and stores it
     def get_player_input
-        @player_input = []
+        @code_breaker_input = []
         @available_colors = Array.new(1) {@MAIN_COLORS}.flatten
         4.times do
             temp_input = request_player_input()
@@ -47,17 +58,17 @@ public
         temp_input
     end
 #validates the player input, if input is invalid prompts the user to input again
-    def validate_player_input(temp_input)
-        if (@available_colors.index(temp_input).class == Integer) == false
+    def validate_player_input(temp_input,comparison_array = @available_colors)
+        if (comparison_array.index(temp_input).class == Integer) == false
             temp_input = request_player_input("Please choose from ONE of the AVAILABLE options >:(")
-            validate_player_input(temp_input)
+            validate_player_input(temp_input, comparison_array)
         else
             temp_input
         end
     end
 #stores player input
     def store_player_input(temp_input)
-        @player_input << @available_colors[@available_colors.index(temp_input)]
+        @code_breaker_input << @available_colors[@available_colors.index(temp_input)]
         @available_colors.delete(temp_input)
     end
 #Checks the player input and compares it with the secret code,
@@ -65,7 +76,7 @@ public
         @matches = []
         temp_row = [] 
         temp_row += @code_row
-        @player_input.each do |item|
+        @code_breaker_input.each do |item|
             if temp_row.include?(item)
                 @matches << "white"
                 temp_row.delete_at(temp_row.index(item))
@@ -76,9 +87,9 @@ public
 #in the secret code
     def correct_positions
         @code_row.each_with_index do |item,index|
-            if @code_row[index] == @player_input[index]
+            if @code_row[index] == @code_breaker_input[index]
                 @matches.shift
-                @matches<< "black"
+                @matches << "black"
             end
         end
     end
@@ -91,7 +102,7 @@ public
 #updates the code with the players input and appropriate results
     def update_board(turn)
         @board[turn][0]=@matches
-        @board[turn][1]=@player_input
+        @board[turn][1]=@code_breaker_input
     end
 #Checks for whether the bot or the player has guessed the secret code
     def check_for_win_condition
@@ -106,20 +117,28 @@ public
             temp_index+=1
         end
     end
-#This method plays a single (1) turn of the game
-    def play_turn(turn)
+#This method plays a single (1) turn of the game if the player is the code breaker
+    def player_turn(turn)
         get_player_input()
         compare_guesses()
         update_board(turn)
         system "clear"
         display_board(turn)
     end
- 
+    
+#this method plays a single (1) turn of the game if the player is the code maker
+    def bot_turn(turn)
+        get_bot_input()
+        compare_guesses()
+        update_board(turn)
+        display_board(turn)
+    end
 end
 
-game = MASTERMIND.new()
 #binding.pry
-#*MAKE A LOOP THAT PLAYS THE GAME 12 TIMES
+puts "Do you wish to be the code breaker or the code maker?"
+player_role = gets.chomp
+game = MASTERMIND.new()
 turn = 0
 while turn<=12
     puts game.code_row
@@ -130,8 +149,10 @@ while turn<=12
         puts "DEFEAT DEFEAT LEMONS I EAT"
         break
     end
-    game.play_turn(turn)
+    if player_role == "code breaker"
+    game.player_turn(turn)
     turn += 1
+    end
     if game.check_for_win_condition == true
         system "clear"
         temp_index = turn-1
