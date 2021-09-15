534a01e282315866f85630b8b73ec14ec942b106
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-09-15 21:17:36 +0300

    Fixed guess matching against the code, rewrote get_player_input and its related methods

--- mastermind.rb
+++ mastermind.rb
@@ -8,6 +8,7 @@ class MASTERMIND
         create_board()
         if player_role == "code maker"
             get_player_input()
+            @bot_colors = Array.new(1) {@MAIN_COLORS}.flatten
             @secret_code_row = Array.new(1) {@code_breaker_input}.flatten
             @code_breaker_input = ''
         else
@@ -21,59 +22,69 @@ public
         temp_column1 = Array.new(4) {"O"}
         temp_column2 = Array.new(4) {"O"}
         @board = [] 
-        12.times do 
+        6.times do 
             @board << [temp_column1,temp_column2,"Turn:#{temp_index+=1}"]
         end
         @board
     end
 #selects 4 random colors to stand for the code
     def secret_code
-        ai_colors = Array.new(1) {@MAIN_COLORS}.flatten
         @secret_code_row = []
         4.times do
+            ai_colors = Array.new(1) {@MAIN_COLORS}.flatten
             temp_color = ai_colors.sample
             ai_colors.delete(temp_color)
             @secret_code_row << temp_color
         end
     end
-#! WRITE THIS LATER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#! DOES NOT WORK YET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     def get_bot_input(turn)
-        temp_color = Array.new(1) {@MAIN_COLORS}
-        temp_index = turn - 1
-        if turn == 1 
-            temp_input = ["red","red","yellow","yellow"]
-        else 
-            previous_guesses = @board[temp_index][1]
-            previous_results = @board[temp_index]
-        end
+
         store_bot_input(temp_input)
     end
 
+        def bot_guess_options(turn)
+        temp_color = Array.new(1) {@MAIN_COLORS}
+        temp_index = turn - 1
+        previous_turn_results = previous_guess_results(turn)
+        previous_turn_guesses = @board[turn-1][1]
+        if turn == 1
+            temp_input = ["red","red","yellow","yellow"]
+        end
+    end
+    def previous_turn_results(turn)
+        temp_index = turn - 1
+        previous_results =[@board[temp_index][0].count("black"),@board[temp_index][0].count("white")]
+        #previous reuslts, previous guesses
+    end
+#Stores the bot input
     def store_bot_input(temp_input)
         @code_breaker_input = temp_input
     end
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #this calls the request_player_input to take the player input, then validates it and stores it
     def get_player_input
-        @code_breaker_input = []
+
         @available_colors = Array.new(1) {@MAIN_COLORS}.flatten
-        4.times do
             temp_input = request_player_input()
             temp_input = validate_player_input(temp_input)
             store_player_input(temp_input)
-        end
     end
 #requests player for input
     def request_player_input(message = "Choose from one of the following colors")
         puts "#{message}"
         p @available_colors
         temp_input = gets.chomp
-        temp_input
+        temp_input.split 
     end
 #validates the player input, if input is invalid prompts the user to input again
     def validate_player_input(temp_input,comparison_array = @available_colors)
-        if (comparison_array.index(temp_input).class == Integer) == false
-            temp_input = request_player_input("Please choose from ONE of the AVAILABLE options >:(")
+        validation_results = []
+        temp_input.each do |input|
+            validation_results << comparison_array.include?(input)
+        end
+        if (validation_results.all?(true) and validation_results.length == 4) == false
+            temp_input = request_player_input("Please choose FOUR of the AVAILABLE colors >:(")
             validate_player_input(temp_input, comparison_array)
         else
             temp_input
@@ -81,18 +92,16 @@ public
     end
 #stores player input
     def store_player_input(temp_input)
-        @code_breaker_input << @available_colors[@available_colors.index(temp_input)]
+        @code_breaker_input = Array.new(1) {temp_input}.flatten
         #@available_colors.delete(temp_input)
     end
 #Checks the player input and compares it with the secret code,
-    def correct_colors(temp_input)
-        temp_row = [] 
-        temp_row += @secret_code_row
-        temp_row.each do |item|
-            if temp_input.include?(item)
+    def correct_colors(temp_row)
+        @code_breaker_input.each do |item|
+            if temp_row.include?(item)
                 @matches.shift
                 @matches << "white"
-                temp_row.delete_at(temp_row.index(item))
+                temp_row[temp_row.index(item)] = "O"
             end
         end
     end
@@ -100,20 +109,21 @@ public
 #in the secret code
     def correct_positions
         @matches = Array.new(4){"O"}
-        temp_input = Array.new(1){@code_breaker_input}.flatten
-        @secret_code_row.each_with_index do |item,index|
-            if @secret_code_row[index] == temp_input[index]
-                temp_input[index] == "O"
+        temp_row = Array.new(1){@secret_code_row}.flatten
+        temp_row.each_with_index do |item,index|
+            if temp_row[index] == @code_breaker_input[index]
+                temp_row[index] = "O"
                 @matches.shift
                 @matches << "black"
             end
         end
-        temp_input
+        puts "#{temp_row}"
+        temp_row
     end
 #compares the players input with the secret code
     def compare_guesses
-        temp_input = correct_positions()
-        correct_colors(temp_input)
+        temp_row = correct_positions()
+        correct_colors(temp_row)
         @matches
     end
 #updates the code with the players input and appropriate results
@@ -157,7 +167,7 @@ puts "Do you wish to be the code breaker or the code maker?"
 player_role = gets.chomp
 game = MASTERMIND.new(player_role)
 turn = 0
-while turn<=12
+while turn<=6
     puts game.secret_code_row
     if player_role == "code breaker"
 
@@ -174,7 +184,7 @@ while turn<=12
         puts "WINNER WINNER CHICKEN DINNER"
         break
     end
-    if (turn == 12)
+    if (turn == 6)
         system "clear"
         temp_index = turn-1
         game.display_board(temp_index)
